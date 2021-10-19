c2a11aff669ab2d286ab76541e77b10e6e9e6b49
carol_rosa <caroline.rosa.silva@gmail.com>
2021-10-18 21:24:43 -0300

    controllers

---
+++ EasyPark/EasyPark/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using EasyPark.Models;
+using EasyPark.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyPark.Controllers
+{
+    public class BaseController<T> : ControllerBase where T : Entity
+    {
+        private readonly BaseIService<T> _service;
+
+        public BaseController(BaseIService<T> service)
+        {
+            _service = service;
+        }
+    }
+}
---
+++ EasyPark/EasyPark/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using EasyPark.Models;
+using EasyPark.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyPark.Controllers
+{
+    public class UserController : BaseController<User>
+    {
+        public UserController(IUserService service) : base(service) { }
+    }
+}
